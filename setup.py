7fbab4a1c26d6db3721789d1f71ec5eb0f3ec984
Krishna Singhal <ylikehits3@gmail.com>
2021-05-25 10:11:57 +0530

    Update to version0.0.8

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ import re
 import setuptools
 
 with open("UsergeAntiSpamApi/__init__.py", encoding="utf-8") as f:
-    version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]    
+    version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="UsergeAntiSpamApi",
     version=version,
     author="Krishna-Singhal",
     author_email="ylikehits3@gmail.com",
-    description="wrapper for https://api.userge.tk",
+    description="Wrapper for [Userge AntiSpam API](https://api.userge.tk)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Krishna-Singhal/userge-federation",
     project_urls={
         "Bug Tracker": "https://github.com/Krishna-Singhal/userge-federation/issues",
